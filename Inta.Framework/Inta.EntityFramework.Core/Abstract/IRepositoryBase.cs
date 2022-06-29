d6b211889a5cf7a1f03e2a1685d455ad7732cec0
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2022-06-29 17:05:32 +0300

    .

--- Inta.Framework/Inta.EntityFramework.Core/Abstract/IRepositoryBase.cs
+++ Inta.Framework/Inta.EntityFramework.Core/Abstract/IRepositoryBase.cs
@@ -1,11 +1,12 @@
 ﻿using Inta.EntityFramework.Core.Model;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Inta.EntityFramework.Core.Abstract
 {
-    public interface IRepositoryBase<TEntity, DataContext>
+    public interface IRepositoryBase<TEntity, TContext> where TEntity : class, IEntity, new() where TContext : DbContext, new()
     {
         DataResult<TEntity> GetById(int id);
         DataResult<TEntity> Get(Expression<Func<TEntity, bool>> filter);
