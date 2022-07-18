0cf74d23590ce413d7d0a1ec0b607c7749c5ff9e
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2022-07-18 13:16:46 +0300

    .

---
+++ Inta.Kurumsal/Inta.Kurumsal.Dto/ComplexType/SearchParameterItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inta.Kurumsal.Dto.ComplexType
+{
+    public class SearchParameterItemDto
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+        public int MergeOperator { get; set; }
+        public int Operator { get; set; }
+    }
+}
---
+++ Inta.Kurumsal/Inta.Kurumsal.Entity/ComplexType/SearchParameterItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inta.Kurumsal.Entity.ComplexType
+{
+    public class SearchParameterItem 
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+        public int MergeOperator { get; set; }
+        public int Operator { get; set; }
+    }
+}
