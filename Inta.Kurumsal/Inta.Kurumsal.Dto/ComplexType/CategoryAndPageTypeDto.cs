9af1fb6225429999822abaa8383d194ac9efa28d
Erdoğan Kaba <erdogankb57@gmail.com>
2022-11-04 23:56:50 +0300

    .

--- Inta.Kurumsal/Inta.Kurumsal.Dto/ComplexType/CategoryAndPageTypeDto.cs
+++ Inta.Kurumsal/Inta.Kurumsal.Dto/ComplexType/CategoryAndPageTypeDto.cs
@@ -10,11 +10,11 @@ namespace Inta.Kurumsal.Dto.ComplexType
     public class CategoryAndPageTypeDto: IDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string CategoryUrl { get; set; }
-        public string CategoryLink { get; set; }
-        public string ControllerName { get; set; }
-        public string ActionName { get; set; }
-        public string ViewName { get; set; }
+        public string? Name { get; set; }
+        public string? CategoryUrl { get; set; }
+        public string? CategoryLink { get; set; }
+        public string? ControllerName { get; set; }
+        public string? ActionName { get; set; }
+        public string? ViewName { get; set; }
     }
 }
